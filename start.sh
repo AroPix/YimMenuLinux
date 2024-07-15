47d3ed95fbe5d958a17807fd456895352188eeb9
aropix <aropix@tutanota.com>
2024-07-15 15:32:00 +0200

    Download YimMenu DLL

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# URL of the DLL
+url="https://github.com/YimMenu/YimMenu/releases/download/nightly/YimMenu.dll"
+
+# Filename to save as
+filename="YimMenu.dll"
+
+# Download the file, overwriting if it exists
+curl -L -o "$filename" "$url"
+
+echo "Downloaded $filename from $url"
