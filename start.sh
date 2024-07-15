e2acbc4dd8e8f3fb8d2e0abe4515ab02c9566c42
aropix <aropix@tutanota.com>
2024-07-15 15:48:57 +0200

    Selector with option for environment variables

--- start.sh
+++ start.sh
@@ -15,7 +15,40 @@ url="https://github.com/YimMenu/YimMenu/releases/download/nightly/YimMenu.dll"
 # Filename to save as
 filename="YimMenu.dll"
 
-# Download the file, overwriting if it exists
-curl -L -o "$filename" "$url"
 
-echo "Downloaded $filename from $url"
+# If Environment variable SKIP_DOWNLOAD is 1, downloading will be skipped
+if [ "$SKIP_DOWNLOAD" == 1 ]; then
+    echo "SKIP_DOWNLOAD is set to 1, skipping download!"
+else
+    curl -L -o "$filename" "$url"
+    echo "Downloaded $filename from $url"
+fi
+
+# Check if GAME_TYPE environment variable is set
+if [ "$GAME_TYPE" == "proton" ]; then
+    download_proton
+    exit 0
+elif [ "$GAME_TYPE" == "wine" ]; then
+    download_wine
+    exit 0
+fi
+
+# Selector logic if GAME_TYPE is not set
+echo "Please select the game type:"
+echo "1. Proton (Steam)"
+echo "2. Wine (Rockstar Games, Epic Games"
+read -p "Enter your choice (1 or 2): " choice
+
+case $choice in
+    1)
+        proton
+        ;;
+    2)
+        wine
+        ;;
+    *)
+        echo "Invalid choice. Please run the script again and select 1 or 2."
+        exit 1
+        ;;
+esac
+
