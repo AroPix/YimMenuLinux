57602f0a3ef3400ac52e8b6d639c9ef1daff22d3
aropix <aropix@tutanota.com>
2024-07-15 17:37:50 +0200

    First working script

--- start.sh
+++ start.sh
@@ -1,34 +1,123 @@
 #!/bin/bash
 
 proton() {
-    echo "Proton logic!"
+    winePath="$HOME/.local/share/Steam/steamapps/common/Grand Theft Auto V/"
+    install
 }
 
 wine() {
-    echo "Wine logic!"
+    # Check if WINE_GTA_PATH environment variable is set
+    if [ -z "$WINE_GTA_PATH" ]; then
+        winePath="$HOME/Games/grand-theft-auto-v/drive_c/Program Files (x86)/Grand Theft Auto V/"
+        echo "WINE_GTA_PATH is not set, using standard Lutris path $winePath"
+    else
+        winePath="$WINE_GTA_PATH"
+    fi
+    install
+}
+
+download_yim() {
+  curl -L -o "$yimDLL" "$url_yim"
+  echo "Downloaded $yimDLL from $url_yim"
+}
+
+download_asi() {
+  curl -L -o "${asiLoader}.zip" "$url_asi"
+  echo "Downloaded ${asiLoader}.zip from $url_asi"
+  unzip -o "${asiLoader}.zip" "${asiLoader}.dll"
+}
+
+install() {
+    if [ ! -d "$winePath" ]; then
+        echo "Game folder doesn't exist!!!"
+        exit 0
+    fi
+
+  if [ ! -f "${winePath}/${asiLoader}Hooked.dll" ]; then
+      cp "${winePath}/${asiLoader}.dll" "./${asiLoader}.Backup.dll"
+      mv "${winePath}/${asiLoader}.dll" "${winePath}/${asiLoader}Hooked.dll"
+      echo "Hooked.dll not found, renaming $asiLoader"
+  fi
+
+  mv "$asiLoader.dll" "$winePath"
+  if [ ! -d "$winePath/scripts" ]; then
+      mkdir -p "$winePath/scripts"
+      echo "The folder $winePath/scripts did not exist and has been created."
+  fi
+  mv $yimDLL "$winePath/scripts"
+
+  echo "Everything is set!"
+  if [ "$gameType" == "proton" ]; then
+    launch_steam
+  fi
+}
+
+launch_steam() {
+  if [ "$LAUNCH_STEAM" == 1 ]; then
+    steam steam://rungameid/271590
+  else
+    read -p "Do you want to launch the game on Steam now? (y/n): " choice
+
+    case $choice in
+        y)
+            steam steam://rungameid/271590 &
+            exit 1
+            ;;
+        *)
+            echo "Exiting..."
+            exit 1
+            ;;
+    esac
+  fi
 }
 
 
-# URL of the DLL
-url="https://github.com/YimMenu/YimMenu/releases/download/nightly/YimMenu.dll"
+# URL of Yim
+url_yim="https://github.com/YimMenu/YimMenu/releases/download/nightly/YimMenu.dll"
+
+# URL of ASI Loader
+url_asi="https://github.com/ThirteenAG/Ultimate-ASI-Loader/releases/download/x64-latest/bink2w64-x64.zip"
 
 # Filename to save as
-filename="YimMenu.dll"
+yimDLL="YimMenu.asi"
+
+# ASILoader file name
+asiLoader="bink2w64"
+
+# ENV var GAME_TYPE "proton" or "wine"
+gameType=$GAME_TYPE
 
 
-# If Environment variable SKIP_DOWNLOAD is 1, downloading will be skipped
-if [ "$SKIP_DOWNLOAD" == 1 ]; then
-    echo "SKIP_DOWNLOAD is set to 1, skipping download!"
+# If Environment variable SKIP_DOWNLOAD_YIM is 1, yim downloading will be skipped if file exists
+if [ "$SKIP_DOWNLOAD_YIM" == 1 ]; then
+    if [ -f "$yimDLL" ]; then
+        echo "SKIP_DOWNLOAD_YIM is set to 1, skipping download!"
+    else
+        echo "SKIP_DOWNLOAD_YIM is set to 1, but file doesn't exist so it will be downloaded anyway!"
+        download_yim
+    fi
 else
-    curl -L -o "$filename" "$url"
-    echo "Downloaded $filename from $url"
+    download_yim
 fi
 
+# If Environment variable SKIP_DOWNLOAD_ASI is 1, yim downloading will be skipped if file exists
+if [ "$SKIP_DOWNLOAD_ASI" == 1 ]; then
+    if [ -f "$asiLoader.zip" ]; then
+        echo "SKIP_DOWNLOAD_ASI is set to 1, skipping download!"
+    else
+        echo "SKIP_DOWNLOAD_ASI is set to 1, but file doesn't exist so it will be downloaded anyway!"
+        download_asi
+    fi
+else
+    download_asi
+fi
+
+
 # Check if GAME_TYPE environment variable is set
-if [ "$GAME_TYPE" == "proton" ]; then
+if [ "$gameType" == "proton" ]; then
     proton
     exit 0
-elif [ "$GAME_TYPE" == "wine" ]; then
+elif [ "$gameType" == "wine" ]; then
     wine
     exit 0
 fi
@@ -41,6 +130,7 @@ read -p "Enter your choice (1 or 2): " choice
 
 case $choice in
     1)
+        gameType="proton"
         proton
         ;;
     2)
